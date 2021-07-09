b86dc0bbea5a276f17e967eb64666fe5bea43421
Danilo Conrado <danilo@premiersoft.net>
2021-07-09 16:56:49 -0300

    add: grid validation function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,14 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
-    private static int[][] table;
+    private static int[][] grid;
 
-    private static boolean loadTable(String csvFilePath) {
+    private static boolean loadGrid(String csvFilePath) {
         try {
             File csvFile = new File(csvFilePath);
             BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFile));
@@ -18,7 +19,7 @@ public class Main {
             String[] splitLine;
             int index = 0;
 
-            table = new int[9][];
+            grid = new int[9][];
 
             while (bufferedReader.ready() || index < 9) {
                 line = bufferedReader.readLine() + ",0"; // hack
@@ -29,12 +30,12 @@ public class Main {
                     return false;
                 }
 
-                table[index] = new int[9];
+                grid[index] = new int[9];
                 for (int i = 0; i < 9; ++i) {
                     if (splitLine[i].isEmpty()) {
-                        table[index][i] = 0;
+                        grid[index][i] = 0;
                     } else {
-                        table[index][i] = Integer.parseInt(splitLine[i]);
+                        grid[index][i] = Integer.parseInt(splitLine[i]);
                     }
                 }
                 ++index;
@@ -53,21 +54,64 @@ public class Main {
         }
     }
 
-    // TODO incomplete
-    private static boolean validSudokuTable() {
-        int sum;
-        ArrayList<Integer> unique = new ArrayList<>(9);
+    private static boolean validSudokuGrid(Boolean isFinalGrid) {
+        Set<Integer> unique = new HashSet<>(9);
 
-        for (int i = 0; i < 9; ++i) {
-            sum = 0;
+        // Check grid lines and validate numbers range
+        int i, j;
+        for (i = 0; i < 9; ++i) {
+            unique.clear();
+            for (j = 0; j < 9; ++j) {
+                if (!isFinalGrid && grid[i][j] == 0) {
+                    continue;
+                }
 
-            for (int j = 0; j < 9; ++j) {
-                sum += table[i][j];
+                if (grid[i][j] < 1 || grid[i][j] > 9) {
+                    System.out.println("Invalid value " + grid[i][j] + " at cell(" + i + ", " + j + ")");
+                    return false;
+                }
+
+                if (!unique.add(grid[i][j])) {
+                    System.out.println("Invalid line " + i + ". Element " + grid[i][j] + " is reoccurring at cell(" + i + ", " + j + ")");
+                    return false;
+                }
             }
+        }
 
-            if (sum != 45) {
-                System.out.println("Invalid sum at row " + (i + 1) + ": " + sum);
-                return false;
+        // Check grid columns
+        for (i = 0; i < 9; ++i) {
+            unique.clear();
+            for (j = 0; j < 9; ++j) {
+                if (!isFinalGrid && grid[j][i] == 0) {
+                    continue;
+                }
+
+                if (!unique.add(grid[j][i])) {
+                    System.out.println("Invalid column " + i + ". Element " + grid[j][i] + " is reoccurring at cell(" + j + ", " + i + ")");
+                    return false;
+                }
+            }
+        }
+
+        // Check grid quadrants
+        int mr, mc;
+        for (int r = 0; r < 9; r += 3) {
+            for (int c = 0; c < 9; c += 3) {
+                unique.clear();
+                mr = r + 3;
+                mc = c + 3;
+                for (i = r; i < mr; ++i) {
+                    for (j = c; j < mc; ++j) {
+                        if (!isFinalGrid && grid[i][j] == 0) {
+                            continue;
+                        }
+
+                        if (!unique.add(grid[i][j])) {
+                            System.out.println("Invalid quadrant " + (r + (c / 3)));
+                            return false;
+                        }
+                    }
+                }
             }
         }
 
@@ -76,19 +120,19 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.print("Missing CSV file path");
+            System.out.println("Missing CSV file path");
             return;
         }
 
         if (args.length > 1) {
-            System.out.print("Too many arguments, expecting only CSV file path");
+            System.out.println("Too many arguments, expecting only CSV file path");
             return;
         }
 
-        if (!loadTable(args[0])) {
+        if (!loadGrid(args[0])) {
             return;
         }
 
-        System.out.println("Valid sudoku table: " + validSudokuTable());
+        System.out.println("Valid sudoku grid: " + validSudokuGrid(true));
     }
 }
